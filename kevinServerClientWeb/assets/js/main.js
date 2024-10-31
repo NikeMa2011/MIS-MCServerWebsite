f24d76adbe62655299324816b9f0d88a56297688
NikeMa <yuanxi.ma@merchiston.cn>
2024-10-31 11:35:08 +0000

    添加单独的菜单样式/JS菜单添加修正

--- kevinServerClientWeb/assets/js/main.js
+++ kevinServerClientWeb/assets/js/main.js
@@ -1,19 +1,22 @@
-const body = document.getElementById("index");
-const blurTitle = document.getElementById("blurTitle");
-const wellcome = document.getElementById("wellcome");
-const hiddenBox = document.getElementById("hiddenBox");
-const container = document.getElementById("container");
 const manu = document.getElementById("manu");
 
 function addManu(){
-    manu.innerHTML = `
-                     <a href="assets/web/" title="主页">
-            <img id="icon" src="assets/image/icon.jpg" alt="主页图标">
-        </a>
-    <div id="links">
-        <a href="assets/web/contributors.html">🎈贡献者名单</a>
-    </div>
-    `;
+    manu.innerHTML 
+    //<div id="manu"> ↖but bro how do it work??
+       = `<a id="serverIcon" href="assets/web/" title="主页">
+                <img id="icon" src="assets/image/icon.jpg" alt="主页图标">
+            </a>
+             <div id="links">
+                   <div id="navigation">
+                    <p class="">导航</p>
+                    <div>
+                        <a href="assets/web/contributors.html">🎈贡献者名单</a>
+                        <a href="assets/web/usefulLinks.html">🔗有用的链接</a>
+                    </div>
+                </div>
+            </div>`;
+    //</div>
 };
 
 addManu();
+
