f668d4cfa2ae2902282f5a68717ade1dd8f62a60
碳酸氢钠一氧化二氢溶液 <yuanxi.ma@merchiston.cn>
2024-11-01 11:29:05 +0000

    更改菜单内容

--- kevinServerClientWeb/assets/js/main.js
+++ kevinServerClientWeb/assets/js/main.js
@@ -2,18 +2,18 @@ const manu = document.getElementById("manu");
 
 function addManu() {
     manu.innerHTML = `
-        <a id="serverIcon" href="" title="主页">
-            <img id="icon" src="assets/image/icon.jpg" alt="主页图标">
-        </a>
-        <div id="links">
+    <div id="links">
             <div id="navigation">
                 <p class="">导航</p>
                 <div id="subArea">
-                    <a href="assets/web/contributors.html">🎈贡献者名单</a>
-                    <a href="assets/web/usefulLinks.html">🔗有用的链接</a>
+                    <a href="contributors.html">🎈贡献者名单</a>
+                    <a href="usefulLinks.html">🔗有用的链接</a>
                 </div>
             </div>
         </div>
+        <a id="serverIcon" href="main.html" title="主页">
+            <img id="icon" src="../image/icon.jpg" alt="主页图标">
+        </a>
     `;
 // 以后请在这修改菜单内容 此脚本会自动为网页加上
 }
